20384c0f15e8d7397c473ce1b638113f2488e121
Joseph Mudd <josephmuddjr@gmail.com>
2023-09-21 19:04:48 -0500

    Code Book constructor and add-to-library function

---
+++ script.js
@@ -0,0 +1,23 @@
+const myLibrary = [];
+
+function Book(title, author, pages, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = (Boolean(haveRead)) ? 'already read' : 'not read yet';
+
+    this.info = function () {
+        return `${title} by ${author}, ${pages} pages, ${this.haveRead}`;
+    }
+}
+
+
+function addBookToLibrary(title, author, pages, haveRead) {
+    const newBook = new Book(title, author, pages, haveRead);
+
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+
+console.table(myLibrary);
